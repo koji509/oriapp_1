c1b79125aae4dd18a03c532a697b956a206eb83c
koji509 <thislifek@yahoo.co.jp>
2021-02-02 13:55:40 +0900

    記述8

--- app/models/fashion.rb
+++ app/models/fashion.rb
@@ -2,6 +2,7 @@ class Fashion < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :comments, dependent: :destroy
+  has_many :users, through: :comments
 
   validates :artist, presence: true
   validates :brand, presence: true
